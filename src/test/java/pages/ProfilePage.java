67ff3d5ca780f82dd4707719d0374c5425dbfc47
saravanabhavanj <saravanaj01@gmail.com>
2020-02-11 23:25:38 +0530

    check

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -1,5 +1,30 @@
 package pages;
 
 public class ProfilePage {
-
+	@FindBy(name = "username")
+	WebElement username;
+	
+	@FindBy(name = "pwd")
+	WebElement password;
+	
+	@FindBy(id = "loginButton")
+	WebElement loginBttn;
+	
+	@FindBy(xpath = "//div[@class='atLogoImg']")
+	WebElement actitimeLogo;
+	
+	public LoginPage(){
+		PageFactory.initElements(driver, this);
+		Reporter.log("Intialized all the elements in Login Page ",true);
+	}
+	
+	public String validatePageTitle(){
+		return driver.getTitle();
+	}
+	
+	public boolean validateActiTimeLogo(){
+		return actitimeLogo.isDisplayed();
+	}
+	
+	
 }
